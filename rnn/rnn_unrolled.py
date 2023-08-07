46a29178fee7b909458377fc3011d57cd0c29cac
Matt Megarry <mattmegarry@gmail.com>
2023-08-07 12:27:59 +0100

    adding reminder file of intention to make an 'unrolled' rnn - for educative purposes

--- rnn/rnn_unrolled.py
+++ rnn/rnn_unrolled.py
@@ -1,7 +1,1 @@
-def dataset(full_seq_len =  200, timesteps = 25):
-    x_train, y_train = [], []
-    sin_wave = np.sin(np.arange(size))
-    for step in range(sin_wave.shape[0]-timesteps):
-        x_train.append(sin_wave[step:step+timesteps])
-        y_train.append(sin_wave[step+timesteps])
-    return np.array(x).reshape(len(y),timesteps,1),np.array(y).reshape(len(y),1)
+# Coming soon - perhaps
