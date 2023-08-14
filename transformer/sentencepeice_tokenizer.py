16c6abb0b5738c69573476f6ca6668eabb40b8c9
Matt Megarry <mattmegarry@gmail.com>
2023-08-14 18:09:52 +0100

    sentencepeice tokenizer

---
+++ transformer/sentencepeice_tokenizer.py
@@ -0,0 +1,23 @@
+from torchtext.data.functional import load_sp_model
+
+class SentencePieceTokenizer:
+    def __init__(self):
+        self.sp_model = load_sp_model("sentencepeice/m.model")  
+
+    def encode(self, text):
+        return self.sp_model.EncodeAsPieces(text)
+    
+    def decode(self, tokens):
+        return self.sp_model.DecodePieces(tokens)
+    
+    def get_vocab(self):
+        return self.sp_model.GetPieceSize()
+
+# Just for testing...
+tokenizer = SentencePieceTokenizer()
+sentence = "Hello, my name is John."
+encoded = tokenizer.encode(sentence)
+decoded = tokenizer.decode(encoded)
+print(encoded)
+print(decoded)  
+print(tokenizer.get_vocab())
