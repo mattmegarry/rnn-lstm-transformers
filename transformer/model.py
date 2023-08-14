2b1df69eebe6ff53d554f7685f8ee6b8c578904c
Matt Megarry <mattmegarry@gmail.com>
2023-08-14 16:45:58 +0100

    [refactor] - split out self attention mechanism

--- transformer/model.py
+++ transformer/model.py
@@ -12,44 +12,18 @@ class DecoderModel(torch.nn.Module):
     self.embedding    = torch.nn.Embedding(self.vocab_len, self.embedding_dimensions)
     self.pos_emb      = self.get_pos_matrix()
 
-    self.register_buffer('mask', torch.tril(torch.ones(max_seq_len, max_seq_len)))
+    self.attn_one = SelfAttention(self.max_seq_len)
+    self.attn_two = SelfAttention(self.max_seq_len)
 
-    self.layer_00_key = torch.nn.Linear(7, 11)
-    self.layer_00_qry = torch.nn.Linear(7, 11)
-    self.layer_00_val = torch.nn.Linear(7, 11)
-    self.layer_00_ffw = torch.nn.Linear(11, 7)
-
-    self.layer_01_key = torch.nn.Linear(7, 11)
-    self.layer_01_qry = torch.nn.Linear(7, 11)
-    self.layer_01_val = torch.nn.Linear(7, 11)
-    self.layer_01_ffw = torch.nn.Linear(11, 7)
-
-    self.map_to_vocab = torch.nn.Linear(7, self.vocab_len)
+    self.map_to_vocab = torch.nn.Linear(self.embedding_dimensions, self.vocab_len)
 
   def forward(self, x):
     emb = self.embedding(x)
     pos = self.pos_emb[0:x.shape[0], :]
     emb = emb + pos
 
-    key = self.layer_00_key(emb)
-    qry = self.layer_00_qry(emb)
-    val = self.layer_00_val(emb)
-    att = torch.mm(qry, key.t())
-    msk = self.mask[0:x.shape[0], 0:x.shape[0]]
-    att = att.masked_fill(msk == 0, float('-inf'))
-    att = torch.nn.functional.softmax(att, dim=1)
-    res = torch.mm(att, val)
-    res = self.layer_00_ffw(res)
-
-    key = self.layer_01_key(res)
-    qry = self.layer_01_qry(res)
-    val = self.layer_01_val(res)
-    att = torch.mm(qry, key.t())
-    msk = self.mask[0:x.shape[0], 0:x.shape[0]]
-    att = att.masked_fill(msk == 0, float('-inf'))
-    att = torch.nn.functional.softmax(att, dim=1)
-    res = torch.mm(att, val)
-    res = self.layer_01_ffw(res)
+    res = self.attn_one(x, emb)
+    res = self.attn_two(x, res)
 
     out = self.map_to_vocab(res)
 
@@ -63,3 +37,26 @@ class DecoderModel(torch.nn.Module):
         store[pos, i] = math.sin(pos / denominator)
         if i + 1 < 7: store[pos, i + 1] = math.cos(pos / denominator)
     return store
+  
+class SelfAttention(torch.nn.Module):
+    def __init__(self, max_seq_len):
+        super(SelfAttention, self).__init__()
+        self.register_buffer('mask', torch.tril(torch.ones(max_seq_len, max_seq_len)))
+        self.key = torch.nn.Linear(7, 11)
+        self.qry = torch.nn.Linear(7, 11)
+        self.val = torch.nn.Linear(7, 11)
+        self.feed_forward = torch.nn.Linear(11, 7)
+
+    def forward(self, x, x_embeddings):
+        key = self.key(x_embeddings)
+        qry = self.qry(x_embeddings)
+        val = self.val(x_embeddings)
+
+        att = torch.mm(qry, key.t())
+        msk = self.mask[0:x.shape[0], 0:x.shape[0]]
+        att = att.masked_fill(msk == 0, float('-inf'))
+        att = torch.nn.functional.softmax(att, dim=1)
+        res = torch.mm(att, val)
+
+        res = self.feed_forward(res)
+        return res
