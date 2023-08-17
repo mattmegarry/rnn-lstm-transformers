cc0cc9c808b653798922a4025c442d6f0144cb69
Matt Megarry <mattmegarry@gmail.com>
2023-08-17 15:01:23 +0100

    gpu setup - for reference

--- gpu-setup/gpu-setup.sh
+++ gpu-setup/gpu-setup.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
-
-# Download and install miniconda
 wget -P ~/ https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x ~/Miniconda3-latest-Linux-x86_64.sh
 ~/Miniconda3-latest-Linux-x86_64.sh -b
-
-# Configure miniconda
 export PATH=~/miniconda3/bin:$PATH 
 conda init
-
-# Disable auto activate base env 
 conda config --set auto_activate_base false
-
-# Create conda environment
 conda create --name gigglers-env python=3.8 -y
-
-# Activate environment
+conda env list
 conda activate gigglers-env
-
-# Install packages
-conda install pipx
+conda list
+pip install pipx
 pipx run nvitop
